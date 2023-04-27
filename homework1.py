25a3e8749dee5f3aabfc3af3e19f26b0a58b0754
Artem Fayda <artem.fayda@mail.ru>
2023-04-27 19:58:34 +0300

    second task from second homework

--- homework1.py
+++ homework1.py
@@ -1,24 +1,36 @@
-coin = int(input("Введите кол-во монеток, лежащих на столе: "))
-side = input("Введите последовательность сторон монеток, где 'r'-решка, а 'g'-герб: ")
-face = 0
-back = 0
-for i in range(coin):
-    if side[i] == 'r':
-        face += 1
-    elif side[i] == 'g':
-        back += 1
-if face > back:
-    print(back)
-elif face == back:
-    print(coin//2)
-else:
-    print(face)
+# coin = int(input("Введите кол-во монеток, лежащих на столе: "))
+# side = input("Введите последовательность сторон монеток, где 'r'-решка, а 'g'-герб: ")
+# face = 0
+# back = 0
+# for i in range(coin):
+#     if side[i] == 'r':
+#         face += 1
+#     elif side[i] == 'g':
+#         back += 1
+# if face > back:
+#     print(back)
+# elif face == back:
+#     print(coin//2)
+# else:
+#     print(face)
+
+print("Загадайте два числа от 0 до 1000")
+sum = int(input("Введите сумму этих двух чисел: "))
+product = int(input("Введите произведение этих двух чисел: "))
+solution = False
+while solution != True:
+    for i in range(1, sum):
+        firstNum = sum - i
+        if firstNum * i == product:
+            print(firstNum, i)
+            solution = True
 
 
-# rightSide = None
-# wrongSide = None
-# if side > otherSide:
-#     for i in otherSide:
+
+
+
+
+
         
         
 
